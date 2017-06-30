df986144c72b6b7a7d96aee631d41ea73d77b0d4
antoniofb3 <antoniofb3>
2017-06-30 14:33:22 -0500

    Adding empowerment score
    Adding empowerment score

--- app/src/main/java/com/antoniofb/seniorityappfinal/empowerment/EmpowermentActivity.java
+++ app/src/main/java/com/antoniofb/seniorityappfinal/empowerment/EmpowermentActivity.java
@@ -1,15 +1,62 @@
 package com.antoniofb.seniorityappfinal.empowerment;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.antoniofb.seniorityappfinal.R;
+import com.antoniofb.seniorityappfinal.results.SummaryActivity;
 
 public class EmpowermentActivity extends AppCompatActivity {
+
+    private String[] empowerment = {"High", "Medium", "Low"};
+    private ListView lvEmpowerment;
+    private int empowermentScore = 0, empowermentPercentage = 10;
+    private int[] scores;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empowerment);
+        setActionBarTitle();
+        showManagementList();
+        acumulateScores();
+    }
+
+    public void setActionBarTitle(){
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Empowerment");
+    }
+
+    public void showManagementList(){
+        lvEmpowerment = (ListView) findViewById(R.id.lvEmpowermentList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.activity_listview,empowerment);
+        lvEmpowerment.setAdapter(adapter);
+        intent = new Intent(this, SummaryActivity.class);
+        lvEmpowerment.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    empowermentScore = 0 * empowermentPercentage;
+                    scores[6] = empowermentScore;
+                    intent.putExtra("EIH", scores);//always put the same key
+                    startActivity(intent);
+                    //startActivity(new Intent(ManagementActivity.this,CommunicationActivity.class));
+                }
+            }
+        });
+    }
+
+    public void acumulateScores(){
+        Bundle bundle = getIntent().getExtras();
+        scores = bundle.getIntArray("LEWE");
+        Log.d("TAG", "Score from leadership experience ->>>>: " + scores[5] + " lalala");
     }
 }
