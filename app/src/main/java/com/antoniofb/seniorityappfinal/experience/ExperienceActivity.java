55e993101dff3b760eab758122c25c62eb8446e1
antoniofb3 <antoniofb3>
2017-07-03 23:47:50 -0500

    Adding scores for experience options
    Adding scores for experience options

--- app/src/main/java/com/antoniofb/seniorityappfinal/experience/ExperienceActivity.java
+++ app/src/main/java/com/antoniofb/seniorityappfinal/experience/ExperienceActivity.java
@@ -17,11 +17,12 @@ import com.antoniofb.seniorityappfinal.results.SummaryActivity;
 
 public class ExperienceActivity extends AppCompatActivity {
 
-    private String[] experience = {"Without Experience", "1 or more year of experience", "2 or more years of experience", "3 or more years of experience", "4 or more years of experience"};
+    private String[] experience = {"Without Experience", "1 or more year of experience", "2 or more years of experience", "3 or more years of experience", "4 or more years of experience", "5 or more years of experience", "6 or more years of experience"};
     private ListView lvExperience;
     private int experienceScore = 0, educationScore = 0, experiencePercentage = 20;
     private int[] scores;
     private Intent intent;
+    private int[] experienceScores = {0,1,2,3,4,5,6,7,8,9,10};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +47,42 @@ public class ExperienceActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0){
-                    experienceScore = 0 * experiencePercentage;
+                    experienceScore = experienceScores[0] * experiencePercentage;
+                    scores[1] = experienceScore;
+                    intent.putExtra("WOE", scores);
+                    startActivity(intent);
+                    //startActivity(new Intent(ExperienceActivity.this,SummaryActivity.class));
+                }
+                if (position == 1){
+                    experienceScore = experienceScores[1] * experiencePercentage;
+                    scores[1] = experienceScore;
+                    intent.putExtra("WOE", scores);
+                    startActivity(intent);
+                    //startActivity(new Intent(ExperienceActivity.this,SummaryActivity.class));
+                }
+                if (position == 2){
+                    experienceScore = experienceScores[2] * experiencePercentage;
+                    scores[1] = experienceScore;
+                    intent.putExtra("WOE", scores);
+                    startActivity(intent);
+                    //startActivity(new Intent(ExperienceActivity.this,SummaryActivity.class));
+                }
+                if (position == 3){
+                    experienceScore = experienceScores[3] * experiencePercentage;
+                    scores[1] = experienceScore;
+                    intent.putExtra("WOE", scores);
+                    startActivity(intent);
+                    //startActivity(new Intent(ExperienceActivity.this,SummaryActivity.class));
+                }
+                if (position == 4){
+                    experienceScore = experienceScores[4] * experiencePercentage;
+                    scores[1] = experienceScore;
+                    intent.putExtra("WOE", scores);
+                    startActivity(intent);
+                    //startActivity(new Intent(ExperienceActivity.this,SummaryActivity.class));
+                }
+                if (position == 5){
+                    experienceScore = experienceScores[5] * experiencePercentage;
                     scores[1] = experienceScore;
                     intent.putExtra("WOE", scores);
                     startActivity(intent);
