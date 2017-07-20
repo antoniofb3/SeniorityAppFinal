409fa32af98ba7d40691a31b8b1ba9e8be9b2aa7
Antonio Flores <aflores@Mac-mini-de-AFLORES.local>
2017-07-20 10:56:38 -0500

    Adding help for technical skills

--- app/src/main/java/com/antoniofb/seniorityappfinal/technicalSkills/TechnicalSkillsActivity.java
+++ app/src/main/java/com/antoniofb/seniorityappfinal/technicalSkills/TechnicalSkillsActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -44,6 +46,31 @@ public class TechnicalSkillsActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         finish();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_technical_skills, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.helpButton:
+                startActivity(new Intent(this,TechnicalSkillsHelpActivity.class));
+                break;
+
+            case android.R.id.home:
+                onBackPressed();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
         return true;
     }
 
