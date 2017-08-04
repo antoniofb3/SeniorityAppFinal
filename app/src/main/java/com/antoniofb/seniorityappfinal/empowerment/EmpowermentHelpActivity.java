6d52ba30cc3f6474d23f42e05bb0ab8f5c77163b
antoniofb3 <antoniofb3>
2017-08-04 14:07:58 -0500

    Reading empowerment help from json

--- app/src/main/java/com/antoniofb/seniorityappfinal/empowerment/EmpowermentHelpActivity.java
+++ app/src/main/java/com/antoniofb/seniorityappfinal/empowerment/EmpowermentHelpActivity.java
@@ -1,17 +1,30 @@
 package com.antoniofb.seniorityappfinal.empowerment;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.antoniofb.seniorityappfinal.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class EmpowermentHelpActivity extends AppCompatActivity {
+
+    private TextView tvFEHelpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empowerment_help);
         setBarOptions();
+        setupGUI();
+        fillTVHelp();
     }
 
     @Override
@@ -24,5 +37,32 @@ public class EmpowermentHelpActivity extends AppCompatActivity {
     public void setBarOptions(){
         getSupportActionBar().setTitle("Help for Empowerment");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void setupGUI(){
+        tvFEHelpText = (TextView) findViewById(R.id.tvFEHelpText);
+    }
+
+    public void fillTVHelp(){
+        Resources resources = getResources();
+        InputStream inputStream = resources.openRawResource(R.raw.factors);
+        Scanner scanner = new Scanner(inputStream);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (scanner.hasNextLine()){
+            stringBuilder.append(scanner.nextLine());
+        }
+        parseJson(stringBuilder.toString());
+    }
+
+    private void parseJson(String s) {
+        try {
+            JSONObject root = new JSONObject(s);
+            JSONObject factor = root.getJSONObject("seniority");
+            JSONArray jsonFactors = factor.getJSONArray("factors");
+            JSONObject jsonObject = jsonFactors.getJSONObject(6);
+            tvFEHelpText.setText(jsonObject.getString("help"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
